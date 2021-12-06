0f3b3fea5771d2ea301ad4c32a0088581df96639
tnotheis <timo.notheisen@js-soft.com>
2021-12-06 16:05:40 +0100

    refactor: extract ServiceHealthCheck to file

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -48,7 +45,6 @@ namespace ApiGateway
             services.AddOcelot(_configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseSecurityHeaders(policies =>
@@ -90,68 +86,7 @@ namespace ApiGateway
                 })
             };
 
-            return httpContext.Response.WriteAsync(JsonSerializer.Serialize(responseObject, new JsonSerializerOptions {WriteIndented = true}));
+            return httpContext.Response.WriteAsync(JsonSerializer.Serialize(responseObject, new JsonSerializerOptions { WriteIndented = true }));
         }
-    }
-
-    public class ServiceHealthCheck : IHealthCheck
-    {
-        private static readonly HttpClient HTTP_CLIENT = new();
-        private readonly IConfiguration _configuration;
-
-        public ServiceHealthCheck(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
-        {
-            var serviceHealthEndpoints = _configuration.GetSection("HealthCheck:Urls").Get<string[]>();
-
-            var tasks = serviceHealthEndpoints.Select(async serviceHealthEndpoint =>
-            {
-                var serviceHost = new Uri(serviceHealthEndpoint).Host;
-
-                try
-                {
-                    var result = await HTTP_CLIENT.GetAsync(serviceHealthEndpoint, cancellationToken);
-
-                    return
-                        new ServiceHealth
-                        {
-                            HealthCheckEndpoint = serviceHost,
-                            StatusCode = result.StatusCode,
-                            Status = result.StatusCode == HttpStatusCode.OK ? "Healthy" : "Unhealthy",
-                            Description = await result.Content.ReadAsStringAsync(cancellationToken)
-                        };
-                }
-                catch (HttpRequestException ex)
-                {
-                    return
-                        new ServiceHealth
-                        {
-                            HealthCheckEndpoint = serviceHost,
-                            StatusCode = ex.StatusCode,
-                            Status = "Unhealthy",
-                            Description = "An unexpected error occured."
-                        };
-                }
-            }).ToList();
-
-            var healthCheckResults = await Task.WhenAll(tasks);
-
-            var resultData = healthCheckResults.ToDictionary(r => r.HealthCheckEndpoint, r => (object) new {r.Status});
-
-
-            return healthCheckResults.All(r => r.StatusCode == HttpStatusCode.OK) ? HealthCheckResult.Healthy("", resultData) : HealthCheckResult.Unhealthy("", null, resultData);
-        }
-    }
-
-    public class ServiceHealth
-    {
-        public string HealthCheckEndpoint { get; set; }
-        public HttpStatusCode? StatusCode { get; set; }
-        public string Status { get; set; }
-        public string Description { get; set; }
     }
 }
