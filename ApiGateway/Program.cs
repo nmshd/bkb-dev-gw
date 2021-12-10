00bd8af15435b179653534c22fea10a6391465bc
Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
2021-12-10 14:33:16 +0100

    Chore/jssnmshdd 2721 upgrade to dotnet 6 (#4)
    * chore: upgrade to dotnet 6
    
    * chore: turn on implicit usings
    
    * chore: fix Dockerfile after image change
    
    * chore: switch to file scoped namespaces
    
    * chore: use top level statements
    
    * chore: resharper cleanup
    
    * ci: fix codeql action
    
    * ci: use + instead of * in version

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,54 +1,42 @@
-﻿using System;
+﻿using ApiGateway;
 using Azure.Identity;
 using Enmeshed.Tooling.Extensions;
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace ApiGateway
+CreateWebHostBuilder(args).Build().Run();
+
+static IWebHostBuilder CreateWebHostBuilder(string[] args)
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    var builder = WebHost.CreateDefaultBuilder(args);
+
+    return builder
+        .UseKestrel(options =>
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            options.AddServerHeader = false;
+            options.Limits.MaxRequestBodySize = 20.Mebibytes();
+        })
+        .ConfigureServices(s => s.AddSingleton(builder))
+        .ConfigureAppConfiguration(AddAzureAppConfiguration)
+        .UseStartup<Startup>();
+}
 
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+static void AddAzureAppConfiguration(WebHostBuilderContext hostingContext, IConfigurationBuilder builder)
+{
+    var configuration = builder.Build();
+
+    builder.AddJsonFile("routing.json", false, true);
+
+    var azureAppConfigurationConfiguration = configuration.GetAzureAppConfigurationConfiguration();
+
+    if (azureAppConfigurationConfiguration.Enabled)
+        builder.AddAzureAppConfiguration(appConfigurationOptions =>
         {
-            var builder = WebHost.CreateDefaultBuilder(args);
+            var credentials = new ManagedIdentityCredential();
 
-            return builder
-                .UseKestrel(options =>
-                {
-                    options.AddServerHeader = false;
-                    options.Limits.MaxRequestBodySize = 20.Mebibytes();
-                })
-                .ConfigureServices(s => s.AddSingleton(builder))
-                .ConfigureAppConfiguration(AddAzureAppConfiguration)
-                .UseStartup<Startup>();
-        }
-
-        private static void AddAzureAppConfiguration(WebHostBuilderContext hostingContext, IConfigurationBuilder builder)
-        {
-            var configuration = builder.Build();
-
-            builder.AddJsonFile("routing.json", false, true);
-
-            var azureAppConfigurationConfiguration = configuration.GetAzureAppConfigurationConfiguration();
-
-            if (azureAppConfigurationConfiguration.Enabled)
-                builder.AddAzureAppConfiguration(appConfigurationOptions =>
-                {
-                    var credentials = new ManagedIdentityCredential();
-
-                    appConfigurationOptions
-                        .Connect(new Uri(azureAppConfigurationConfiguration.Endpoint), credentials)
-                        .ConfigureKeyVault(vaultOptions => { vaultOptions.SetCredential(credentials); })
-                        .Select("*", "")
-                        .Select("*", "ApiGateway");
-                });
-        }
-    }
+            appConfigurationOptions
+                .Connect(new Uri(azureAppConfigurationConfiguration.Endpoint), credentials)
+                .ConfigureKeyVault(vaultOptions => { vaultOptions.SetCredential(credentials); })
+                .Select("*", "")
+                .Select("*", "ApiGateway");
+        });
 }
