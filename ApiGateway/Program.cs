e5a81382c2f9384bda3446571d8400b6d66959f3
tnotheis <timo.notheisen@js-soft.com>
2021-12-10 10:10:20 +0100

    chore: use top level statements

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,49 +1,42 @@
-﻿using Azure.Identity;
+﻿using ApiGateway;
+using Azure.Identity;
 using Enmeshed.Tooling.Extensions;
 using Microsoft.AspNetCore;
 
-namespace ApiGateway;
+CreateWebHostBuilder(args).Build().Run();
 
-public class Program
+static IWebHostBuilder CreateWebHostBuilder(string[] args)
 {
-    public static void Main(string[] args)
-    {
-        CreateWebHostBuilder(args).Build().Run();
-    }
+    var builder = WebHost.CreateDefaultBuilder(args);
 
-    private static IWebHostBuilder CreateWebHostBuilder(string[] args)
-    {
-        var builder = WebHost.CreateDefaultBuilder(args);
+    return builder
+        .UseKestrel(options =>
+        {
+            options.AddServerHeader = false;
+            options.Limits.MaxRequestBodySize = 20.Mebibytes();
+        })
+        .ConfigureServices(s => s.AddSingleton(builder))
+        .ConfigureAppConfiguration(AddAzureAppConfiguration)
+        .UseStartup<Startup>();
+}
 
-        return builder
-            .UseKestrel(options =>
-            {
-                options.AddServerHeader = false;
-                options.Limits.MaxRequestBodySize = 20.Mebibytes();
-            })
-            .ConfigureServices(s => s.AddSingleton(builder))
-            .ConfigureAppConfiguration(AddAzureAppConfiguration)
-            .UseStartup<Startup>();
-    }
+static void AddAzureAppConfiguration(WebHostBuilderContext hostingContext, IConfigurationBuilder builder)
+{
+    var configuration = builder.Build();
 
-    private static void AddAzureAppConfiguration(WebHostBuilderContext hostingContext, IConfigurationBuilder builder)
-    {
-        var configuration = builder.Build();
+    builder.AddJsonFile("routing.json", false, true);
 
-        builder.AddJsonFile("routing.json", false, true);
+    var azureAppConfigurationConfiguration = configuration.GetAzureAppConfigurationConfiguration();
 
-        var azureAppConfigurationConfiguration = configuration.GetAzureAppConfigurationConfiguration();
+    if (azureAppConfigurationConfiguration.Enabled)
+        builder.AddAzureAppConfiguration(appConfigurationOptions =>
+        {
+            var credentials = new ManagedIdentityCredential();
 
-        if (azureAppConfigurationConfiguration.Enabled)
-            builder.AddAzureAppConfiguration(appConfigurationOptions =>
-            {
-                var credentials = new ManagedIdentityCredential();
-
-                appConfigurationOptions
-                    .Connect(new Uri(azureAppConfigurationConfiguration.Endpoint), credentials)
-                    .ConfigureKeyVault(vaultOptions => { vaultOptions.SetCredential(credentials); })
-                    .Select("*", "")
-                    .Select("*", "ApiGateway");
-            });
-    }
+            appConfigurationOptions
+                .Connect(new Uri(azureAppConfigurationConfiguration.Endpoint), credentials)
+                .ConfigureKeyVault(vaultOptions => { vaultOptions.SetCredential(credentials); })
+                .Select("*", "")
+                .Select("*", "ApiGateway");
+        });
 }
